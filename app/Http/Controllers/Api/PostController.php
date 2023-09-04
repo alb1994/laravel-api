3484b8969b40f1b4c366e63bbeb577f48ef76f18
alb1994 <antonetti_alberto@libero.it>
2023-09-04 18:20:11 +0200

    introdotta paginazione

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\post;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(){
-        $posts = post::all();
-        return response()->json([
-            'succes' => true,
-            'results' =>$posts
+       //$posts = Post::all(); 
+       $posts = Post::with('category', 'tags')->paginate(4);
+       return response()->json([
+           'success' => true, 
+            'results' => $posts
         ]); 
     }
 }
